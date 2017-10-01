29ba33dfb4cb89c01473d92d7b7dea6a1001b121
Nan Den <nannandenden@gmail.com>
2017-10-01 14:49:51 -0700

    implement DBFlow to support the offline mode

--- app/src/main/java/com/codepath/apps/restclienttemplate/network/MyDatabase.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/network/MyDatabase.java
@@ -1,7 +1,10 @@
 package com.codepath.apps.restclienttemplate.network;
 
 import com.raizlabs.android.dbflow.annotation.Database;
-
+// @Database annotation generates a DatabaseDefinition which now references your SQLite Database
+// on disk in the file named RestClientDatabase
+// DBFlow supports any number of database, however individual tables and other related files can
+// only be associated with one database
 @Database(name = MyDatabase.NAME, version = MyDatabase.VERSION)
 public class MyDatabase {
 
--- app/src/main/java/com/codepath/apps/restclienttemplate/utils/Utils.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/utils/Utils.java
@@ -1,6 +1,9 @@
 package com.codepath.apps.restclienttemplate.utils;
 
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.text.format.DateUtils;
 import android.util.Log;
 
@@ -17,6 +20,7 @@ public class Utils {
     private static final String LOG_TAG = Utils.class.getSimpleName();
     // get relative time ago
     public static String getRelativeTimeAgo(String rawJsonData) {
+        if (rawJsonData.contains("ago")) return rawJsonData;
         String twitterFormat = "EEE MMM dd HH:mm:ss ZZZZZ yyyy";
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat(twitterFormat, Locale.ENGLISH);
         simpleDateFormat.setLenient(true);
@@ -30,6 +34,16 @@ public class Utils {
             Log.d(LOG_TAG, e.getMessage());
             e.printStackTrace();
         }
+        Log.d(LOG_TAG, relativeDate);
         return relativeDate;
+    }
+
+    public static boolean isNetworkAvailable(Context context) {
+        ConnectivityManager cm =
+                (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        return activeNetwork != null &&
+                activeNetwork.isConnectedOrConnecting();
     }
 }
